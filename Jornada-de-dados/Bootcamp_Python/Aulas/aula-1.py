b3d3dcc441a95f3533ee5abeb9a8668987012c75
GabrielCStabile <devstabile@gmail.com>
2025-09-06 20:32:19 -0300

    A1 - bootcamp de Python

---
+++ Jornada-de-dados/Bootcamp_Python/Aulas/aula-1.py
@@ -0,0 +1,12 @@
+# Crie um programa que receba o nome do usuário e retorne o nº de caracteres
+# do nome informado.
+
+nome: str = input ("Digite o seu nome: ")
+print (f"O nome {nome} tem {len(nome)} letras.")
+
+# Tipos primitivos em Python (TypeHint)
+
+numero: int = 3 
+decimal: float = 1.5
+nome: str = "Gabriel"
+verdadeiro: bool = True
