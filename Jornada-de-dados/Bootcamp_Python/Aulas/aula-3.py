731bb04841c7cb7efa15a8d25fbfcbdd7eae729f
GabrielCStabile <devstabile@gmail.com>
2025-09-08 20:33:51 -0300

    Aula 3: Debugger, Controle de Fluxo, Lista e Dicionário.

--- Jornada-de-dados/Bootcamp_Python/Aulas/aula-3.py
+++ Jornada-de-dados/Bootcamp_Python/Aulas/aula-3.py
@@ -18,7 +18,16 @@ else:
 for i in range (1,5):
     print(i)
 
+# Debugando e iterando sobre um range
+# Ele vai do primeiro número mas não chega no último 
+# Output (1, 2, 3, 4)    
+
 lista_jogos = ["Devil May Cry", "Resident Evil", "Silent Hill"]
 
 for jogo in lista_jogos:
     print(jogo)
+
+# Iterando e Debugando uma lista
+# Ele imprime todos os itens da lista e depois finaliza a execução
+
+
