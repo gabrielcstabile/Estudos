d710b6634cad94ab1ab083913f4a55430add9b00
GabrielCStabile <devstabile@gmail.com>
2025-09-07 22:50:46 -0300

    Aula 2 - Bootcamp de Python.

--- Jornada-de-dados/Bootcamp_Python/Aulas/aula-2.py
+++ Jornada-de-dados/Bootcamp_Python/Aulas/aula-2.py
@@ -3,12 +3,13 @@
 try:
     n1: int = int(input("Insira um nº inteiro: "))
     n2: int = int(input("Insira o 2º nº inteiro: "))
-    resultado: int = n1 // n2
-    print (resultado)
+    resultado: int = n1 // n2    
+except ValueError:
+    print("Digite apenas números inteiros.")
 except ZeroDivisionError:
     print("Não pode dividir um número por zero.")
 except KeyboardInterrupt:
-            print("\n Operação cancelada pelo usuário (Ctrl + C).")
-            exit(1)  
-except TypeError as e:
-       print(e)
+    print("\nOperação cancelada pelo usuário (Ctrl + C).")
+    exit(1)
+else:
+    print(f"O resultado da divisão inteira de {n1} por {n2} é {resultado}")
