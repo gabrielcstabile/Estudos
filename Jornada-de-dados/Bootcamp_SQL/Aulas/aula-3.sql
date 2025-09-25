0bbd650ad8b8d29c1bd3f44917b61f9db436276a
GabrielCStabile <devstabile@gmail.com>
2025-09-24 21:53:14 -0300

    Finalizando aula 3

--- Jornada-de-dados/Bootcamp_SQL/Aulas/aula-3.sql
+++ Jornada-de-dados/Bootcamp_SQL/Aulas/aula-3.sql
@@ -11,4 +11,32 @@ FROM orders AS o
 INNER JOIN customers AS c ON o.customer_id = c.customer_id
 WHERE DATE_PART('YEAR', o.order_date) = 1996
 
--- Exercício 2: 
+-- Exercício 2: Criar um relatório que mostra o número de funcionários e clientes de cada cidade que tem funcionários
+SELECT e.city AS cidade, 
+       COUNT(DISTINCT e.employee_id) AS numero_de_funcionarios, 
+       COUNT(DISTINCT c.customer_id) AS numero_de_clientes
+FROM employees e 
+LEFT JOIN customers c ON e.city = c.city
+GROUP BY e.city
+ORDER BY cidade;
+
+-- Criar um relatório que mostra o número de funcionários e clientes de cada cidade que tem clientes
+SELECT c.city AS cidade, 
+       COUNT(DISTINCT c.customer_id) AS numero_de_clientes, 
+       COUNT(DISTINCT e.employee_id) AS numero_de_funcionarios
+FROM employees e 
+RIGHT JOIN customers c ON e.city = c.city
+GROUP BY c.city
+ORDER BY cidade
+
+-- Criar um relatório que mostra o número de funcionários e clientes de cada cidade
+SELECT
+	COALESCE(e.city, c.city) AS cidade,
+	COUNT(DISTINCT e.employee_id) AS numero_de_funcionarios,
+	COUNT(DISTINCT c.customer_id) AS numero_de_clientes
+FROM employees e 
+FULL JOIN customers c ON e.city = c.city
+GROUP BY e.city, c.city
+ORDER BY cidade
+
+
