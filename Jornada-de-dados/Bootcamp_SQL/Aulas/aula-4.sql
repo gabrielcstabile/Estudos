6cb85133735915b545b3455f3e37f1b221bb2808
GabrielCStabile <devstabile@gmail.com>
2025-10-06 22:42:08 -0300

    Aula 4 SQL

--- Jornada-de-dados/Bootcamp_SQL/Aulas/aula-4.sql
+++ Jornada-de-dados/Bootcamp_SQL/Aulas/aula-4.sql
@@ -10,5 +10,14 @@ SELECT
 	SUM (unit_pricE * quantity) OVER (PARTITION BY order_id) AS total_sales
 FROM order_details
 ORDER BY order_id
+
+-- Quais são os valores mínimo, máximo e médio de frete pago por cada cliente 
+-- Tabela Orders 
 	
-	
+	SELECT customer_id,
+		MIN(freight) as min_freight,
+		MAX(freight) as max_freight,
+		AVG(freight) as avg_freight
+FROM orders
+GROUP BY customer_id
+ORDER BY customer_id
