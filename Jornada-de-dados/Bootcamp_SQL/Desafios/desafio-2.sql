710af0f2c1c87529589254a59525261c55c62b08
GabrielCStabile <devstabile@gmail.com>
2025-10-13 19:06:26 -0300

    Add D2 SQL

--- Jornada-de-dados/Bootcamp_SQL/Desafios/desafio-2.sql
+++ Jornada-de-dados/Bootcamp_SQL/Desafios/desafio-2.sql
@@ -1,11 +1,37 @@
 -- 1. Obter todas as colunas das tabelas Clientes, Pedidos e Fornecedores
 
+SELECT * FROM customers;
+SELECT * FROM orders;
+SELECT * FROM suppliers;
+
 -- 2. Obter todos os Clientes em ordem alfabética por país e nome
+
+SELECT *
+FROM customers
+ORDER BY country, contact_name;     
 
 -- 3. Obter os 5 pedidos mais antigos
 
+SELECT *
+FROM orders
+ORDER BY order_date ASC
+LIMIT 5;
+
 -- 4. Obter a contagem de todos os Pedidos feitos durante 1997
+
+SELECT COUNT(*) AS total_pedidos_1997
+FROM orders
+WHERE order_date BETWEEN '1997-01-01' AND '1997-12-31';
 
 -- 5. Obter os nomes de todas as pessoas de contato onde a pessoa é um gerente, em ordem alfabética
 
+SELECT contact_name
+FROM customers
+WHERE contact_title LIKE '%Manager%' OR
+      contact_title LIKE '%Owner%'
+ORDER BY contact_name
+       
 -- 6. Obter todos os pedidos feitos em 19 de maio de 1997
+SELECT order_id
+FROM orders
+WHERE order_date = '1997-05-19'
